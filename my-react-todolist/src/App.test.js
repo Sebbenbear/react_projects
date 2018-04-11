741cd0179ca9ee4d8b5086a97fb7c4f69da6cc96
Pauline <pauline.m.kelly1@gmail.com>
2018-04-11 15:58:51 +1000

    Configure so all tests now work

--- my-react-todolist/src/App.test.js
+++ my-react-todolist/src/App.test.js
@@ -1,49 +1,10 @@
-/* global expect, test */
+/* global it, expect */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-const sum = (a, b) => a + b;
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('is equal to this object', () => {
-  const data = { hello: 'world' };
-  data.myNum = 19;
-  expect(data).toEqual({ hello: 'world', myNum: 19 });
-});
-
-test('adding floating point numbers', () => {
-  const value = 0.1 + 0.2;
-  expect(value).toBeCloseTo(0.3);
-});
-
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/);
-});
-
-test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
-});
-
-const shoppingList = [
-  'diapers',
-  'kleenex',
-  'trash bags',
-  'paper towels',
-  'beer',
-];
-
-test('the shopping list has beer on it', () => {
-  expect(shoppingList).toContain('beer');
-});
-
-
